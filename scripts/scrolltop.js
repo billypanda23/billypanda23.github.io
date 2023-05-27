0bc4ea291c4ea086df9a7f1d722daaf765669d6a
billypanda23 <wilberrubik@gmail.com>
2023-05-27 14:20:50 +0800

    simplify

--- scripts/scrolltop.js
+++ scripts/scrolltop.js
@@ -1,3 +1,4 @@
+// 001
 // When the user scrolls down 1500px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -13,4 +14,71 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+
+// 002
+// sideNavigation
+function openNav() {
+    document.getElementById("mySidenav").style.width = "180px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// 003
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+// 004
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+function myFunctionDrop() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//for 2nd dropdown
+function myFunctionDrop2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+  }
+  
+  
+  if (!event.target.matches('.dropbtn2')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show2')) {
+            openDropdown.classList.remove('show2');
+        }
+    }
+  }  
 }
